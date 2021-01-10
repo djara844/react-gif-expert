75be62f4098e2ff9c9a13c716f999d5811c0ca02
Diego Jaramillo <djaramillop7@gmail.com>
2021-01-10 07:02:24 -0500

    Add docs folder

---
+++ docs/precache-manifest.d0c87d848dee5d4c17cd99738c8a3a26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3ed31dbbf98b37bdd22700fd1145a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8784af3f489f07f72a49",
+    "url": "/static/css/main.00c306fe.chunk.css"
+  },
+  {
+    "revision": "baebdb037d157b7a160b",
+    "url": "/static/js/2.6b4199b8.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.6b4199b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8784af3f489f07f72a49",
+    "url": "/static/js/main.016b2b4d.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
